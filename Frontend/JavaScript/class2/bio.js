a9efcaee2ce6bad50d7aa276dcdb0af7121dc805
soumya <sd7557920@gmail.com>
2024-10-07 12:00:14 +0530

    TEMPLATE LITERAL

---
+++ Frontend/JavaScript/class2/bio.js
@@ -0,0 +1,8 @@
+// todo :print your name ,age ,roll_no,college,batch,city,in template literal
+let name = "soumya";
+let city = "bhubaneswar";
+let roll_no = 177;
+let college = "united school of business management";
+let batch = 2025;
+let varr = `my name is ${name},i am from ${city},Roll_no is:${roll_no},and i am pursuing my MCA from ${college} ,${city} ${batch}`;
+console.log(varr);
--- Frontend/JavaScript/class2/data.js
+++ Frontend/JavaScript/class2/data.js
@@ -1,27 +1,53 @@
-let data;
-console.log(typeof data); //undefined
-console.log(data);
-//number
-let num1 = 50;
-let decimal_num = 1.0001;
-console.log(typeof num1); //number
-console.log(typeof decimal_num); //number
-let pi = 3.141;
-let age = 21;
-let roll_num = 117;
-console.log();
-// todo adddition,multiplication,division,substraction then push it into your github account.once you done share me
-//addition
-let num3 = 20,
-  num2 = 50;
-let addition = num2 + num3;
-console.log(addition);
-//multiplicatio
-let mul = num3 * num2;
-console.log(mul);
-//division
-let division = num3 / num3;
-console.log(division);
-//substraction
-let sub = num3 - num2;
-console.log(sub);
+// // let data;
+// // console.log(typeof data); //undefined
+// // console.log(data);
+// // //number
+// // let num1 = 50;
+// // let decimal_num = 1.0001;
+// // console.log(typeof num1); //number
+// // console.log(typeof decimal_num); //number
+// // let pi = 3.141;
+// // let age = 21;
+// // let roll_num = 117;
+// // console.log();
+// // todo adddition,multiplication,division,substraction then push it into your github account.once you done share me
+// //addition
+// let num3 = 20,
+//   num2 = 50;
+// let addition = num2 + num3;
+// console.log(addition);
+// //multiplicatio
+// let mul = num3 * num2;
+// console.log(mul);
+// //division
+// let division = num3 / num3;
+// console.log(division);
+// //substraction
+// let sub = num3 - num2;
+// console.log(sub);
+
+// //NaN- Not a number
+// let num5 = 0;
+// let num6 = 0;
+// let div = num5 / num6;
+// console.log(div);
+// let no = 23;
+// let no1 = 0;
+// let div2 = no / no1;
+// let div3 = no1 / no;
+// console.log(div);
+// console.log(div3);
+
+// let name = " soumya ranjan das ";
+// let age = 21;
+// let varr = `my name is ${name} and my age ${age} years old`;
+// console.log(varr);
+
+// todo :print your name ,age ,roll_no,college,batch,city,in template literal
+let name = "soumya";
+let city = "bhubaneswar";
+let roll_no = 177;
+let college = "united school of business management";
+let batch = 2025;
+let varr = `my name is ${name},i am from ${city},Roll_no is:${roll_no},and i am pursuing my MCA from ${college} ,${city} ${batch}`;
+console.log(varr);
