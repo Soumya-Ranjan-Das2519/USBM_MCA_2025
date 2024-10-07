8015ef83cbc5dbd836df6a47d9d3b76aeccee380
soumya <sd7557920@gmail.com>
2024-10-07 11:15:42 +0530

    basic operation

---
+++ Frontend/JavaScript/class2/data.js
@@ -0,0 +1,27 @@
+let data;
+console.log(typeof data); //undefined
+console.log(data);
+//number
+let num1 = 50;
+let decimal_num = 1.0001;
+console.log(typeof num1); //number
+console.log(typeof decimal_num); //number
+let pi = 3.141;
+let age = 21;
+let roll_num = 117;
+console.log();
+// todo adddition,multiplication,division,substraction then push it into your github account.once you done share me
+//addition
+let num3 = 20,
+  num2 = 50;
+let addition = num2 + num3;
+console.log(addition);
+//multiplicatio
+let mul = num3 * num2;
+console.log(mul);
+//division
+let division = num3 / num3;
+console.log(division);
+//substraction
+let sub = num3 - num2;
+console.log(sub);
